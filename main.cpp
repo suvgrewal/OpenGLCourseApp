95898af48dde8ae7b8c2411326cefffc14a4d246
suvgrewal <grewalsuvraj@gmail.com>
2025-09-29 04:02:42 -0500

    Triangle created

--- main.cpp
+++ main.cpp
@@ -7,24 +7,141 @@
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600; // GLint type is typedef for OpenGL
 
-GLuint VAO, VBO, shader; // Vertex Array Object, Vertex Buffer Object
+// Vertex Array Object, Vertex Buffer Object and Shader
+GLuint VAO;
+GLuint VBO;
+GLuint shader;
+
+const int POSITION_COMPONENTS = 3; // x, y, z
+const int TRIANGLE_VERTEX_COUNT = 3; // triangle has 3 vertices
+
+// Vertex Shader
+static const char* vShader = "                                         \n\
+#version 330                                                           \n\
+                                                                       \n\
+layout (location = 0) in vec3 pos;                                     \n\
+                                                                       \n\
+void main()                                                            \n\
+{                                                                      \n\
+    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);                      \n\
+}                                                                      \n\
+";
+
+static const char* fShader = "                                         \n\
+#version 330                                                           \n\
+																	   \n\
+out vec4 color;                                                        \n\
+																	   \n\
+void main()                                                            \n\
+{                                                                      \n\
+	color = vec4(0.0, 0.0, 0.0, 1.0);                                  \n\
+}                                                                      \n\
+";
 
 void CreateTriangle()
 {
-	GLfloat vertices[] = {
-		0.0f,  0.5f, 0.0f, // Vertex 1 (X, Y)
-		0.5f, -0.5f, 0.0f, // Vertex 2 (X, Y)
-	   -0.5f, -0.5f, 0.0f  // Vertex 3 (X, Y)
+	GLfloat vertices[TRIANGLE_VERTEX_COUNT * POSITION_COMPONENTS] = {
+		-1.0f, -1.0f, 0.0f, // Bottom left
+		 1.0f, -1.0f, 0.0f, // Bottom right
+		 0.0f,  1.0f, 0.0f  // Top middle
 	};
-	glGenVertexArrays(1, &VAO); // Generate 1 vertex array object
-	glBindVertexArray(VAO);     // Bind the vertex array object
-	glGenBuffers(1, &VBO);                      // Generate 1 vertex buffer object
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);         // Bind the vertex buffer object
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Copy the vertices to the buffer
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0); // Set the vertex attribute pointer
-	glEnableVertexAttribArray(0); // Enable the vertex attribute array
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the VBO
-	glBindVertexArray(0);             // Unbind the VAO
+
+	GLsizei num_tris = 1;
+	glGenVertexArrays(num_tris, &VAO); // Generate 1 vertex array object
+	glBindVertexArray(VAO);    // Bind the vertex array object
+
+	glGenBuffers(num_tris, &VBO); // Generate 1 vertex buffer object
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the vertex buffer object
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Set buffer data
+
+	GLuint index = 0; // Vertex attribute index
+	GLint vertexSize = 3; // Number of components per vertex attribute (e.g. , x, y, z for 3D position or vec3)
+	GLenum type = GL_FLOAT; // Data type of each component
+	GLboolean isNormalized = GL_FALSE; // Whether fixed-point data values should be normalized
+	GLsizei stride = 0; // Byte offset between consecutive vertex attributes
+	void* offset = (void*)0; // Offset of the first component
+
+	glVertexAttribPointer(index, vertexSize, type, isNormalized, stride, offset); // Set vertex attribute pointer
+	glEnableVertexAttribArray(index); // Enable the vertex attribute array
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the VBO (the call to glVertexAttribPointer registered VBO as the currently bound GL_ARRAY_BUFFER)
+	
+	glBindVertexArray(0); // Unbind the VAO
+}
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+	GLuint theShader = glCreateShader(shaderType); // Create shader object
+
+	if (!theShader) // If shader creation failed
+	{
+		printf("Error creating shader type %d\n", shaderType);
+		return;
+	}
+
+	const GLchar* theCode[1]; // Hold shaderCode in array of pointers
+	theCode[0] = shaderCode;  // Pointer to the shader code string
+
+	GLint codeLength[1];      // Array of lengths of the strings
+	codeLength[0] = strlen(shaderCode); // Length of the shader code string
+	
+	glShaderSource(theShader, 1, theCode, codeLength); // Set the source code in the shader object
+	glCompileShader(theShader); // Compile the shader
+
+	const int logLength = 1024;
+	GLint result = 0;               // Compilation result
+	GLchar eLog[logLength] = { 0 };      // Error log buffer
+	GLsizei* infoLogLength = NULL;
+
+	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result); // Get compilation status
+	
+	if (!result)  // Compilation failed
+	{
+		glGetShaderInfoLog(theShader, sizeof(eLog), infoLogLength, eLog);
+		printf("Error compiling the %d shader: '%s'\n", shaderType, eLog);
+		return;
+	}
+
+
+	glAttachShader(theProgram, theShader); // Attach the compiled shader to the program
+}
+void CompileShaders()
+{
+	shader = glCreateProgram(); // Create shader program
+
+	if (!shader)  // If shader program creation failed
+	{
+		printf("Shader program creation failed!\n");
+		return;
+	}
+
+	AddShader(shader, vShader, GL_VERTEX_SHADER);    // Add vertex shader
+	AddShader(shader, fShader, GL_FRAGMENT_SHADER);  // Add fragment shader
+
+	const int logLength = 1024;
+	GLint result = 0;               // Compilation result
+	GLchar eLog[logLength] = { 0 }; // Error log buffer
+	GLsizei* infoLogLength = NULL;
+
+	glLinkProgram(shader); // Link the shader program; create executables on graphics card to link program together
+	glGetProgramiv(shader, GL_LINK_STATUS, &result); // Get link status
+
+	if (!result)  // Linking failed
+	{
+		glGetProgramInfoLog(shader, logLength, infoLogLength, eLog);
+		printf("Error linking program: '%s'\n", eLog);
+		return;
+	}
+
+	glValidateProgram(shader); // Validate the shader program
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result); // Get validation status
+
+	if (!result)
+	{
+		glGetProgramInfoLog(shader, logLength, infoLogLength, eLog);
+		printf("Error validating program: '%s'\n", eLog);
+		return;
+	}
 }
 
 int main()
@@ -75,6 +192,12 @@ int main()
 
 	glViewport(0, 0, bufferWidth, bufferHeight); // Set the viewport size (0,0) to the size of the window
 	
+	CreateTriangle(); // Create triangle
+	CompileShaders(); // Compile and link shaders
+
+	const int first = 0;
+	const int count = 3;
+
 	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow))
 	{
@@ -83,7 +206,14 @@ int main()
 
 		// Clear window
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Set clear color to red
-		glClear(GL_COLOR_BUFFER_BIT);                  // clear bits
+		glClear(GL_COLOR_BUFFER_BIT);     
+		
+		glUseProgram(shader); // clear bits
+		glBindVertexArray(VAO); // Bind the VAO (the triangle)
+
+		glDrawArrays(GL_TRIANGLES, first, count);
+		glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the VBO
+		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow); // Swap the front and back buffers
 	}
